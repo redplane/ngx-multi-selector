93a7efb3df161a73421b57c0e2800fcc3e1e3c10
Linh Nguyen <redplane_dt@yahoo.com.vn>
2017-10-14 15:39:14 +0800

    - Refactored code.

--- src/components/app/app.routing.ts
+++ src/components/app/app.routing.ts
@@ -1,17 +1,9 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-
 import {AppComponent} from './app.component';
 import {CustomerServiceMock} from "../../services/mocks/customer-service.mock";
 import {ApiServiceMock} from "../../services/mocks/api-service.mock";
-import {NgxMultiSelectorModule} from 'modules/ngx-multi-selector/ngx-multi-selector.module';
 import {RouterModule, Routes} from "@angular/router";
 import {LayoutComponent} from "./shared/layout/layout.component";
-import {SharedModule} from "./shared/shared.module";
-import {BasicInitializationModule} from "./basic-initialization/basic-initialization.module";
-import {AdvancedInitializationModule} from "./advanced-initialization/advanced-initialization.module";
 
 //#region Properties
 
